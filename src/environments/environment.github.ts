782ee6272a763f7b50399c08d352c9c720ad1889
Alva Chien <alvachien@163.com>
2017-09-01 19:39:08 +0800

    Prepare to deploy to github

--- src/environments/environment.github.ts
+++ src/environments/environment.github.ts
@@ -2,12 +2,5 @@ export const environment = {
     production: true,
     DebugLogging: false,
     LoggingLevel: 1, // Crash
-    LoginRequired: false,
-
-    IDServerUrl: 'http://118.178.58.187:5100/',
-    APIBaseUrl: 'http://118.178.58.187:5330/api/',
-    AppLoginCallbackUrl: 'http://118.178.58.187:5230/logincallback.html',
-    AppLoginSlientRevewCallbackUrl: 'http://118.178.58.187:5230/silentrenewcallback.html',
-    AppLogoutCallbackUrl: 'http://118.178.58.187:5230/index.html',
-    AppHost: 'http://118.178.58.187:5230'
+    LoginRequired: false
 };
