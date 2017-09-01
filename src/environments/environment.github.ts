3dc5afb24c712ff643fed8456262ee2bbc1013e5
Alva Chien <alvachien@163.com>
2017-09-01 19:41:26 +0800

    Add empty URL to make the build work for github deployment

--- src/environments/environment.github.ts
+++ src/environments/environment.github.ts
@@ -2,5 +2,12 @@ export const environment = {
     production: true,
     DebugLogging: false,
     LoggingLevel: 1, // Crash
-    LoginRequired: false
-};
+    LoginRequired: false,
+
+    IDServerUrl: '',
+    APIBaseUrl: '',
+    AppLoginCallbackUrl: '',
+    AppLoginSlientRevewCallbackUrl: '',
+    AppLogoutCallbackUrl: '',
+    AppHost: ''
+  };
