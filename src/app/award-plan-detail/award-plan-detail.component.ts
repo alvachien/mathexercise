0ac8082297970cfee557cdce37aed6da3d3ec9db
Alva Chien <alvachien@163.com>
2018-05-01 21:16:58 +0800

    Add new codes

--- src/app/award-plan-detail/award-plan-detail.component.ts
+++ src/app/award-plan-detail/award-plan-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, merge, of as observableOf } from 'rxjs';
+import { map, startWith, switchMap, catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+import { AwardPlan, QuizTypeEnum, GetAllQuizTypeUIStrings, LogLevel, DateFormat,
+  UserDetailInfo, UIMode, QuizTypeUI } from '../model';
 
 @Component({
   selector: 'app-award-plan-detail',
@@ -6,6 +11,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./award-plan-detail.component.scss']
 })
 export class AwardPlanDetailComponent implements OnInit {
+  private _curPlan: AwardPlan;
+  get CurrentPlan(): AwardPlan {
+    return this._curPlan;
+  }
 
   constructor() { }
 
