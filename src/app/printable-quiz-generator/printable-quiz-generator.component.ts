cd17106f2c972a520dbec2f2c92a44af538e0430
Alva Chien <alvachien@163.com>
2018-06-29 19:09:00 +0800

    Add new codes

--- src/app/printable-quiz-generator/printable-quiz-generator.component.ts
+++ src/app/printable-quiz-generator/printable-quiz-generator.component.ts
@@ -4,18 +4,32 @@ import * as jsPDF from 'jspdf';
 @Component({
   selector: 'app-printable-quiz-generator',
   templateUrl: './printable-quiz-generator.component.html',
-  styleUrls: ['./printable-quiz-generator.component.scss']
+  styleUrls: ['./printable-quiz-generator.component.scss'],
 })
 export class PrintableQuizGeneratorComponent implements OnInit {
-  amountQuiz: number;
-  additionRate: number;
+  amountAddQuiz: number;
+  amountSubQuiz: number;
+  amountMulQuiz: number;
+  amountDivQuiz: number;
+  decimalPlaces: number;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onSubmit(): void {
-    // Do nothing
+  onGenerate(): void {
+    // Generate
+    const doc: any = new jsPDF();
+
+    // All units are in the set measurement for the document
+    // This can be changed to "pt" (points), "mm" (Default), "cm", "in"
+    // doc.fromHTML($('body').get(0), 15, 15, {
+    //   'width': 170,
+    //   'elementHandlers': specialElementHandlers
+    // });
+
+    doc.text('Hello world!', 1, 1);
+    doc.save('test.pdf');
   }
 }
