2eba73a0c6c58850178359b426de2f815575c0a6
Alva Chien <alvachien@163.com>
2018-06-30 19:27:37 +0800

    Add new codes

--- src/app/printable-quiz-generator/printable-quiz-generator.component.ts
+++ src/app/printable-quiz-generator/printable-quiz-generator.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as jsPDF from 'jspdf';
-import { AdditionQuizItem, SubtractionQuizItem } from '../model';
+import { AdditionQuizItem, SubtractionQuizItem, MultiplicationQuizItem, DivisionQuizItem } from '../model';
 
 @Component({
   selector: 'app-printable-quiz-generator',
@@ -13,11 +13,17 @@ export class PrintableQuizGeneratorComponent implements OnInit {
   amountMulQuiz: number;
   amountDivQuiz: number;
   decimalPlaces: number;
+  numberBegin: number;
+  numberEnd: number;
 
   constructor() { 
     this.amountAddQuiz = 12;
     this.amountSubQuiz = 12;
+    this.amountMulQuiz = 12;
+    this.amountDivQuiz = 12;
     this.decimalPlaces = 2;
+    this.numberBegin = 1;
+    this.numberEnd = 100;
   }
 
   ngOnInit() {
@@ -26,6 +32,8 @@ export class PrintableQuizGeneratorComponent implements OnInit {
   onGenerate(): void {
     let arAdd: AdditionQuizItem[] = [];
     let arSub: SubtractionQuizItem[] = [];
+    let arMul: MultiplicationQuizItem[] = [];
+    let arDiv: DivisionQuizItem[] = [];
 
     if (this.amountAddQuiz > 0) {
       let idx: number = 0;
@@ -41,49 +49,134 @@ export class PrintableQuizGeneratorComponent implements OnInit {
         arSub.push(qi);
       } while (idx ++ < this.amountSubQuiz);
     }
+    if (this.amountMulQuiz > 0) {
+      let idx: number = 0;
+      do {
+        let qi: MultiplicationQuizItem = this.generateMulQuizItem(idx);
+        arMul.push(qi);
+      } while (idx ++ < this.amountMulQuiz);
+    }
+    if (this.amountDivQuiz > 0) {
+      let idx: number = 0;
+      do {
+        let qi: DivisionQuizItem = this.generateDivQuizItem(idx);
+        arDiv.push(qi);
+      } while (idx ++ < this.amountDivQuiz);
+    }
     const itemsPerRow: number = 3;
 
     // Generate
     const doc: any = new jsPDF();
+    doc.setFontSize(10);
 
     // Generate the codes
     let nrow: number = 0;
-    let nypos: number = 0;
+    let nypos: number = 10;
     if (arAdd.length > 0) {
-      nrow = Math.ceil(arAdd.length / itemsPerRow);
+
+      doc.text("1. Additional Quiz", 10, nypos);
+      nypos += 10;
+
+      nrow = Math.ceil(this.amountAddQuiz / itemsPerRow);
       for(let idx2 = 0; idx2 < nrow; idx2 ++) {
         let ielem: number = idx2 * itemsPerRow;
-        if (ielem < arAdd.length) {
-          doc.text(arAdd[ielem].getQuizFormat(), 10, 10 * (1 + idx2));  
+        if (ielem < this.amountAddQuiz) {
+          doc.text(arAdd[ielem].getQuizFormat(), 5, nypos);  
+          ielem ++;
         }
-        ielem ++;
-        if (ielem < arAdd.length) {
-          doc.text(arAdd[ielem].getQuizFormat(), 70, 10 * (1 + idx2));  
+        if (ielem < this.amountAddQuiz) {
+          doc.text(arAdd[ielem].getQuizFormat(), 70, nypos);  
+          ielem ++;
         }
-        ielem ++;
-        if (ielem < arAdd.length) {
-          doc.text(arAdd[ielem].getQuizFormat(), 130, 10 * (1 + idx2));  
+        if (ielem < this.amountAddQuiz) {
+          doc.text(arAdd[ielem].getQuizFormat(), 135, nypos);  
+          ielem ++;
+        }
+
+        nypos += 10;
+        if (nypos > 280) {
+          nypos = 10;
+          doc.addPage();
         }
       }
-      nypos += nrow;
     }
     if (arSub.length > 0) {
-      nrow = Math.floor(arSub.length / itemsPerRow);
+      doc.text("2. Subtraction Quiz", 10, nypos);
+      nypos += 10;
+
+      nrow = Math.ceil(this.amountSubQuiz / itemsPerRow);
       for(let idx2 = 0; idx2 < nrow; idx2 ++) {
         let ielem: number = idx2 * itemsPerRow;
-        if (ielem < arSub.length) {
-          doc.text(arSub[ielem].getQuizFormat(), 10, 10 * (nypos + 1 + idx2));  
+        if (ielem < this.amountSubQuiz) {
+          doc.text(arSub[ielem].getQuizFormat(), 5, nypos);  
+          ielem ++;
         }
-        ielem ++;
-        if (ielem < arSub.length) {
-          doc.text(arSub[ielem].getQuizFormat(), 70, 10 * (nypos + 1 + idx2));  
+        if (ielem < this.amountSubQuiz) {
+          doc.text(arSub[ielem].getQuizFormat(), 70, nypos);  
+          ielem ++;
         }
-        ielem ++;
-        if (ielem < arSub.length) {
-          doc.text(arSub[ielem].getQuizFormat(), 130, 10 * (nypos + 1 + idx2));  
+        if (ielem < this.amountSubQuiz) {
+          doc.text(arSub[ielem].getQuizFormat(), 135, nypos);  
+          ielem ++;
+        }
+        nypos += 10;
+        if (nypos > 280) {
+          nypos = 10;
+          doc.addPage();
         }
       }
-      nypos += nrow;
+    }
+    if (arMul.length > 0) {
+      doc.text("3. Multiplication Quiz", 10, nypos);
+      nypos += 10;
+
+      nrow = Math.ceil(this.amountMulQuiz / itemsPerRow);
+      for(let idx2 = 0; idx2 < nrow; idx2 ++) {
+        let ielem: number = idx2 * itemsPerRow;
+        if (ielem < this.amountMulQuiz) {
+          doc.text(arMul[ielem].getQuizFormat(), 5, nypos);  
+          ielem ++;
+        }
+        if (ielem < this.amountMulQuiz) {
+          doc.text(arMul[ielem].getQuizFormat(), 70, nypos);  
+          ielem ++;
+        }
+        if (ielem < this.amountMulQuiz) {
+          doc.text(arMul[ielem].getQuizFormat(), 135, nypos);  
+          ielem ++;
+        }
+        nypos += 10;
+        if (nypos > 280) {
+          nypos = 10;
+          doc.addPage();
+        }
+      }
+    }
+    if (arDiv.length > 0) {
+      doc.text("4. Division Quiz", 10, nypos);
+      nypos += 10;
+
+      nrow = Math.ceil(this.amountDivQuiz / itemsPerRow);
+      for(let idx2 = 0; idx2 < nrow; idx2 ++) {
+        let ielem: number = idx2 * itemsPerRow;
+        if (ielem < this.amountDivQuiz) {
+          doc.text(arDiv[ielem].getQuizFormat(), 5, nypos);  
+          ielem ++;
+        }
+        if (ielem < this.amountDivQuiz) {
+          doc.text(arDiv[ielem].getQuizFormat(), 70, nypos);  
+          ielem ++;
+        }
+        if (ielem < this.amountDivQuiz) {
+          doc.text(arDiv[ielem].getQuizFormat(), 135, nypos);  
+          ielem ++;
+        }
+        nypos += 10;
+        if (nypos > 280) {
+          nypos = 10;
+          doc.addPage();
+        }
+      }
     }
 
     doc.save('test.pdf');
@@ -91,18 +184,50 @@ export class PrintableQuizGeneratorComponent implements OnInit {
 
   // Generate add
   private generateAddQuizItem(idx: number): AdditionQuizItem {
-    const rnum1 = Math.random() * (1000 - 1) + 1;
-    const rnum2 = Math.random() * (1000 - 1) + 1;
+    const rnum1 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    const rnum2 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
     const qz: AdditionQuizItem = new AdditionQuizItem(rnum1, rnum2, 2);
     qz.QuizIndex = idx;
     return qz;
   }
   // Generate subtract
   private generateSubQuizItem(idx: number): SubtractionQuizItem {
-    const rnum1 = Math.random() * (1000 - 1) + 1;
-    const rnum2 = Math.random() * (1000 - 1) + 1;
-    const qz: SubtractionQuizItem = new SubtractionQuizItem(rnum1, rnum2, 2);
+    const rnum1 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    const rnum2 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    if (rnum1 > rnum2) {
+      const qz: SubtractionQuizItem = new SubtractionQuizItem(rnum1, rnum2, 2);
+      qz.QuizIndex = idx;
+      return qz;  
+    } else {
+      const qz: SubtractionQuizItem = new SubtractionQuizItem(rnum2, rnum1, 2);
+      qz.QuizIndex = idx;
+      return qz;  
+    }
+  }
+  // Generate multiply
+  private generateMulQuizItem(idx: number): MultiplicationQuizItem {
+    const rnum1 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    const rnum2 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    const qz: MultiplicationQuizItem = new MultiplicationQuizItem(
+      rnum1, rnum2, 2
+    );
     qz.QuizIndex = idx;
     return qz;
-  }  
+  }
+  // Generate div
+  private generateDivQuizItem(nIdx: number): DivisionQuizItem {
+    const rnum1 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    const rnum2 = Math.random() * (this.numberEnd - this.numberBegin) + this.numberBegin;
+    if (rnum1 > rnum2) {
+      const dq: DivisionQuizItem = new DivisionQuizItem(rnum1, rnum2, 2);
+      dq.QuizIndex = nIdx;
+  
+      return dq;  
+    } else {
+      const dq: DivisionQuizItem = new DivisionQuizItem(rnum2, rnum1, 2);
+      dq.QuizIndex = nIdx;
+  
+      return dq;  
+    }
+  }
 }
